abd844ee946d27ca0e05efe139a4514776b7cb2b
albertogiuntelli-hue <alberto.giuntelli@gmail.com>
2026-03-02 11:09:00 +0100

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,17 @@
+// backend/config/db.js
+
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+export default pool;
