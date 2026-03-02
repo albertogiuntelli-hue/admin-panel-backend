f83d4bf7996d6893cce72e9f7c776f1f1db3ccb8
albertogiuntelli-hue <alberto.giuntelli@gmail.com>
2026-03-02 12:16:58 +0100

    Delete data/middleware directory

--- data/middleware/admin.js
+++
@@ -1,6 +0,0 @@
-export default function (req, res, next) {
-    if (!req.user || !req.user.isAdmin) {
-        return res.status(403).json({ message: "Accesso riservato agli admin." });
-    }
-    next();
-}
